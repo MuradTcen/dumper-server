efbfeb289686afa908f1fb5267d5099ab35c544a
MuradTcen <muradtcen@.gmail.com>
2020-07-29 10:51:31 +0500

    repaired Query

--- src/main/java/com/dumper/server/enums/Query.java
+++ src/main/java/com/dumper/server/enums/Query.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Query {
 
-    BACKUP("\"BACKUP DATABASE [database] TO DISK = N'directory' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10\""),
-    DIFFERENTIAL_BACKUP("\"BACKUP DATABASE [database] TO DISK = N'directory' WITH DIFFERENTIAL, NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10\""),
-    RESTORE("\"RESTORE DATABASE [database] FROM DISK = N'directory'\"");
+    BACKUP("BACKUP DATABASE [database] TO DISK = N'directory' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"),
+    DIFFERENTIAL_BACKUP("BACKUP DATABASE [database] TO DISK = N'directory' WITH DIFFERENTIAL, NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"),
+    RESTORE("RESTORE DATABASE [database] FROM DISK = N'directory'");
 
     private final String query;
 }
