fe4afdd63a9466328a8d011aef8fca095198faf0
MuradTcen <muradtcen@.gmail.com>
2020-08-13 07:17:26 +0500

    added infrastructure files

--- src/main/java/com/dumper/server/ServerApplication.java
+++ src/main/java/com/dumper/server/ServerApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @Configuration
 @EnableConfigurationProperties
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class ServerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/dumper/server/repository/BackupsetRepository.java
@@ -0,0 +1,14 @@
+package com.dumper.server.repository;
+
+import com.dumper.server.entity.Backupset;
+import com.dumper.server.entity.BackupsetTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface BackupsetRepository extends JpaRepository<BackupsetTest, Long> {
+    List<Backupset> getAllByDatabaseNameAndBackupStartDateAfter(String databaseName, LocalDateTime backupStartDate);
+}
