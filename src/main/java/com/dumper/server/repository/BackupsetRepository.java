073cf18fd0ad0d3d781b6c3b3475a1f513bceef1
MuradTcen <muradtcen@.gmail.com>
2020-08-16 22:26:22 +0500

    added query to BackupsetRepository

--- src/main/java/com/dumper/server/repository/BackupsetRepository.java
+++ src/main/java/com/dumper/server/repository/BackupsetRepository.java
@@ -3,6 +3,7 @@ package com.dumper.server.repository;
 import com.dumper.server.entity.Backupset;
 import com.dumper.server.entity.BackupsetTest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -12,5 +13,15 @@ import java.util.List;
 public interface BackupsetRepository extends JpaRepository<BackupsetTest, Long> {
     List<Backupset> getAllByDatabaseNameAndBackupStartDateAfter(String databaseName, LocalDateTime backupStartDate);
 
-    List<Backupset> getAllByDatabaseNameAndBackupStartDateAfterAndOrderByDatabaseBackupLsnAsc(String databaseName, LocalDateTime backupStartDate);
+    @Query(
+            value = "WITH full_dump (lsn, date)\n" +
+                    "    AS (select top 1 b.first_lsn, b.backup_start_date from backupset as b where type='D' order by backup_start_date desc)\n" +
+                    "\n" +
+                    "    select bs.first_lsn, bs.last_lsn,bs.checkpoint_lsn, bs.database_backup_lsn, bm.physical_device_name, bs.type\n" +
+                    "    from backupset as bs\n" +
+                    "    inner join backupmediafamily as bm on bs.media_set_id = bm.media_set_id\n" +
+                    "    where bs.database_name = ?1 and (bs.database_backup_lsn = (select lsn from full_dump) or" +
+                    " bs.first_lsn = (select lsn from full_dump)) and bs.backup_start_date >= (select date from full_dump)",
+            nativeQuery = true)
+    List<Object[]> getDumpsByDatabaseAndDate(String databaseName);
 }
