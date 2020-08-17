7b879710b5d323f6b5c216c7faf6dad77a3cbfdf
MuradTcen <muradtcen@.gmail.com>
2020-08-18 02:20:03 +0500

    changed query

--- src/main/java/com/dumper/server/repository/BackupsetRepository.java
+++ src/main/java/com/dumper/server/repository/BackupsetRepository.java
@@ -11,13 +11,14 @@ import java.util.List;
 public interface BackupsetRepository extends JpaRepository<Backupset, Long> {
 
     @Query(
-            value = "with full_dump (lsn, date)\n" +
-                    "    as (select top 1 b.first_lsn, b.backup_start_date from backupset as b where type='D' order by backup_start_date desc)\n" +
+            value = "with full_dump (lsn, backup_lsn, date)\n" +
+                    "    as (select top 1 b.first_lsn, b.database_backup_lsn, b.backup_start_date from backupset as b where type='D' order by backup_start_date desc)\n" +
                     "\n" +
                     "    select bs.first_lsn, bs.last_lsn,bs.checkpoint_lsn, bs.database_backup_lsn, bm.physical_device_name, bs.type\n" +
                     "    from backupset as bs\n" +
                     "    inner join backupmediafamily as bm on bs.media_set_id = bm.media_set_id\n" +
                     "    where bs.database_name = ?1 and (bs.database_backup_lsn = (select lsn from full_dump) or" +
+                    " bs.database_backup_lsn = (select backup_lsn from full_dump) or" +
                     " bs.first_lsn = (select lsn from full_dump)) and bs.backup_start_date >= (select date from full_dump)",
             nativeQuery = true)
     List<Object[]> getDumpsByDatabase(String databaseName);
