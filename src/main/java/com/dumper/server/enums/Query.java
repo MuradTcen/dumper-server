586f93f7451033d7522f5caed582f4ff68952cb8
MuradTcen <muradtcen@.gmail.com>
2020-08-18 02:22:51 +0500

    added queries enum

--- src/main/java/com/dumper/server/enums/Query.java
+++ src/main/java/com/dumper/server/enums/Query.java
@@ -8,10 +8,15 @@ import lombok.RequiredArgsConstructor;
 public enum Query {
 
     VERSION("SELECT @@VERSION"),
+
     FULL_BACKUP("BACKUP DATABASE [database] TO DISK = N'directory' WITH COPY_ONLY, STATS = 10"),
     DIFFERENTIAL_BACKUP("BACKUP DATABASE [database] TO DISK = N'directory' WITH DIFFERENTIAL, STATS = 10"),
-    RESTORE("RESTORE DATABASE [database] FROM DISK = N'directory' WITH REPLACE"),
-    RESTORE_DIFFERENTIAL("RESTORE DATABASE [database] FROM DISK = N'directory' WITH NORECOVERY");
+
+    RESTORE_FULL("RESTORE DATABASE [database] FROM DISK = N'directory' WITH REPLACE"),
+    RESTORE_DIFFERENTIAL("RESTORE DATABASE [database] FROM DISK = N'directory' WITH NORECOVERY"),
+
+    LOG_BACKUP("BACKUP LOG [database] TO DISK = N'directory' WITH NOFORMAT, INIT, STATS = 10"),
+    RESTORE_LOG("RESTORE LOG [database] FROM DISK = N'directory' WITH STATS = 10");
 
     private final String query;
 }
